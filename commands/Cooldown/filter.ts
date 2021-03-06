579247daa822d0262269b9c6b949cc9e6d8ac9a3
DevSamuelV <realalloutmusic@gmail.com>
2021-03-06 13:58:57 -0500

    Fixed the freeze admin in freeze.ts I need to add it to filter.ts

--- commands/Cooldown/filter.ts
+++ commands/Cooldown/filter.ts
@@ -2,29 +2,30 @@ import discord from "discord.js";
 import { db } from "../../db/db";
 
 export class CooldownFilter {
-  private frozenUsers: IFrozenUser[] = [];
+	private frozenUsers: IFrozenUser[] = [];
 
-  constructor(cli: discord.Client) {
-    cli.on("message", async (message) => {
-      const author = message.author.username.toLowerCase();
-      const frozenUsers = await db.freeze.Get();
+	constructor(cli: discord.Client) {
+		cli.on("message", async (message) => {
+			const author = message.author.username.toLowerCase();
+			const frozenUsers = await db.freeze.Get();
 
-      this.frozenUsers = frozenUsers;
+			this.frozenUsers = frozenUsers;
 
-      for (var i = 0; i != this.frozenUsers.length; i++) {
-        const user = this.frozenUsers[i].username.toLowerCase();
+			for (var i = 0; i != this.frozenUsers.length; i++) {
+				const user = this.frozenUsers[i].username.toLowerCase();
 
-        if (author == user) {
-          if (!message.deletable) return;
+				if (author == user) {
+					if (user == "pinky" || user == "developer" || user == "crew") return;
+					if (!message.deletable) return;
 
-          message.author
-            .createDM()
-            .then((channel) => channel.send("Your Frozen 🥶"))
-            .catch((err) => console.log(`${err.message} ${author}`));
+					message.author
+						.createDM()
+						.then((channel) => channel.send("Your Frozen 🥶"))
+						.catch((err) => console.log(`${err.message} ${author}`));
 
-          return message.delete();
-        }
-      }
-    });
-  }
+					return message.delete();
+				}
+			}
+		});
+	}
 }
--- commands/Cooldown/freeze.ts
+++ commands/Cooldown/freeze.ts
@@ -3,54 +3,58 @@ import { db } from "../../db/db";
 import { Json } from "../../util/Json";
 
 export class Freeze {
-  private command = "#freeze";
-  private frozenUsers: string[] = [];
+	private command = "#freeze";
+	private frozenUsers: string[] = [];
 
-  private modRoleId = "631240392779759628";
-  private adminRoleId = "723228470720856167";
+	private modRoleId = "631240392779759628";
+	private adminRoleId = "723228470720856167";
 
-  constructor(cli: discord.Client) {
-    cli.on("message", async (message) => {
-      if (!message.toString().includes(this.command)) return;
+	constructor(cli: discord.Client) {
+		cli.on("message", async (message) => {
+			if (!message.toString().includes(this.command)) return;
 
-      const content = message.content;
-      const pre = content.split(this.command)[1];
-      const arg = pre.split(" ");
+			const content = message.content;
+			const pre = content.split(this.command)[1];
+			const arg = pre.split(" ");
 
-      const user = arg[1];
-      const authorRoles = message.member?.roles.cache;
+			const user = arg[1];
+			const authorRoles = message.member?.roles.cache;
 
-      if (authorRoles == null) return message.reply("You Don't have any roles");
+			if (authorRoles == null) return message.reply("You Don't have any roles");
 
-      if (content.includes("#help")) return;
+			if (content.includes("#help")) return;
 
-      for (var i = 0; i != authorRoles.array().length; i++) {
-        const role = authorRoles.array()[i];
-        const id = role.id;
+			for (var i = 0; i != authorRoles.array().length; i++) {
+				const role = authorRoles.array()[i];
+				const id = role.id;
 
-        if (
-          id == this.adminRoleId ||
-          id == this.modRoleId ||
-          id == "681326153969172529"
-        ) {
-          const frozenUsers = await db.freeze.Get();
+				if (
+					id == this.adminRoleId ||
+					id == this.modRoleId ||
+					id == "681326153969172529"
+				) {
+					const frozenUsers = await db.freeze.Get();
 
-          for (var i = 0; i != frozenUsers.length; i++) {
-            const frUser = this.frozenUsers[i];
+					for (var i = 0; i != frozenUsers.length; i++) {
+						const frUser = this.frozenUsers[i];
 
-            if (user == frUser)
-              return message.reply(`${user} is already frozen!`);
-          }
+						if (user == frUser)
+							return message.reply(`${user} is already frozen!`);
+					}
 
-          await db.freeze.Add(user);
+					if (user == "pinky" || user == "developer" || user == "crew") {
+						return message.reply("no");
+					}
 
-          return message.reply(`${user} is now frozen 🥶`);
-        }
-      }
+					await db.freeze.Add(user);
 
-      return message.reply(
-        "Im Sorry you don't have the roles to freeze someone."
-      );
-    });
-  }
+					return message.reply(`${user} is now frozen 🥶`);
+				}
+			}
+
+			return message.reply(
+				"Im Sorry you don't have the roles to freeze someone."
+			);
+		});
+	}
 }
