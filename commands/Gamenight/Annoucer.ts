90fb94496b2c90e10a348974244cc18f06d7e563
DevSamuelV <realalloutmusic@gmail.com>
2021-03-12 13:20:23 -0500

    updated announcer.ts

--- commands/Gamenight/Annoucer.ts
+++ commands/Gamenight/Annoucer.ts
@@ -5,12 +5,14 @@ export class Announcer {
 	private ChannelId = "807268738684551209";
 
 	constructor(cli: Client) {
-		const channel = cli.channels.cache.get(this.ChannelId);
+		const channel: Channel = cli.channels.cache.get(this.ChannelId)!;
 
 		const job = new CronJob(
 			"1 33 14 * * 5",
 			() => {
 				if (channel == null) return;
+
+				channel.send();
 			},
 			null,
 			true,
--- commit.py
+++
@@ -1,6 +0,0 @@
-import os
-
-comment = input("commit message: ")
-
-os.system('git add .')
-os.system('git commit -m "' + comment + '"')
